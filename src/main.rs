7bb887e134c4a34693528d2d4302e0c5e1698954
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-07-11 21:20:37 +0200

    dont use strings

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-fn main() {
-    let input = "0x4000, 0x4000, 0x5800, 0x6400, 0x4400, 0x4400, 0x6400, 0x5800, 0x0000, 0x0000,";
-    let mut strings = Vec::new();
+fn draw_char(input: &str) {
+    let mut values = Vec::new();
     for value in input.split(", ") {
         let value: u16 =
             u16::from_str_radix(value.trim_start_matches("0x").trim_end_matches(","), 16).unwrap();
-        let text = format!("{value:016b}");
-        // println!("{}", text.replace("0", " ").replace("1", "█"));
-        strings.push(text);
+        values.push(value);
     }
 
-    for y in (0..strings.len()).step_by(2) {
-        for x in 0..strings[0].len() {
-            let top_pixel = &strings[y as usize][x as usize..x as usize + 1] == "1";
-
-            let bottom_pixel = &strings[y as usize + 1][x as usize..x as usize + 1] == "1";
+    for y in (0..values.len()).step_by(2) {
+        for x in 0..16 {
+            let top_pixel = &values[y as usize] << x & 0x4000 != 0;
+            let bottom_pixel = &values[y as usize + 1] << x & 0x4000 != 0;
             if top_pixel && bottom_pixel {
                 print!("█");
             } else if top_pixel {
@@ -26,4 +22,7 @@ fn main() {
         }
         println!("");
     }
+}
+fn main() {
+    draw_char("0x4000, 0x4000, 0x5800, 0x6400, 0x4400, 0x4400, 0x6400, 0x5800, 0x0000, 0x0000,")
 }
